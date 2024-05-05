2d5048ad5c361712b0cc880644ec4868b2863c56
lxiang0725 <106471184+lxiang0725@users.noreply.github.com>
2024-05-05 20:54:46 +0800

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,47 @@
+/* app.js
+* main script file for our little application
+* */
+
+"use strict";
+
+$(document).ready(function(){
+    $('p a[href!="#top"]').attr('target', '_blank');
+    $('section').hide().fadeIn(1000);
+
+    $('nav a, p a[href="#top"]').click(function(eventObject){
+        console.log(this.hash);
+        var targetElement = jQuery(this.hash);
+        $('html,body').animate({
+            scrollTop: targetElement.offset().top - navHeight
+        }, 700);
+
+        eventObject.preventDefault();
+    });
+
+    var nav = $('nav');
+    var navTop = nav.offset().top;
+    var navHeight = nav.outerHeight();
+    var navPlaceholder = $('.nav-placeholder');
+    navPlaceholder.height(navHeight);
+
+    $(window).scroll(function(){
+        var scrollPos = $(this).scrollTop();
+        console.log(scrollPos);
+        if(scrollPos > navTop) {
+            nav.addClass('nav-fixed');
+            navPlaceholder.show();
+        }else{
+            nav.removeClass('nav-fixed');
+            navPlaceholder.hide();
+        }
+    });
+
+    $('#exit-button').click(function(){
+       $('#confirm-exit-modal').modal();
+    });
+
+    $('#confirm-exit-button').click(function(){
+        window.location = 'http://chrissumption.com/';
+    });
+
+});
